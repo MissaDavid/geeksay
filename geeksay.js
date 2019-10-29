ef5d0b2ace5630b6515f3a2ff8da80d64e3aea52
Melissa David <mei.chohee@gmail.com>
2019-10-29 20:40:44 +1100

    Update geeksay.js
    Issue #5 : add more quotes

--- geeksay.js
+++ geeksay.js
@@ -1,6 +1,7 @@
 const translations = {
     "save": "ctrl+s",
-    "break": "<br>",
+    "
+    ": "<br>",
     "control": "ctrl",
     "rule": "ctrl",
     "escape": "esc",
@@ -121,6 +122,8 @@ const translations = {
     "waiting": "loading",
     "forbidden": "403",
     "unauthorized": "401",
+    "shrug": "¯\_(ツ)_/¯",
+    "true": "!false",
 }
 
 const quotes = [
@@ -166,6 +169,9 @@ const quotes = [
     "keep calm and geek on", // keep calm && g33k 1
     "somebody save me", // somebody ctrl+s me
     "Easy as pie", // easy as π
+    "shrug it off", // ¯\_(ツ)_/¯ it off
+    "I'm not your help button", // I'm not your f1 button
+    "This is true", // this is !false
 ]
 
 function isNumeric(num) {
